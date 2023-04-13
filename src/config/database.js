219b7c376714882c835bdbfa634bc081e53505a5
Andres Parra <52136174+Andres-Parra2610@users.noreply.github.com>
2023-04-13 15:55:02 -0400

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise')
 
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    port: 3306,
-    database: 'higea'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE
 })
 
 module.exports = pool
